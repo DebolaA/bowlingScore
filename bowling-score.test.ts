445901198e981ca08a63b5ceb278a7a824729355
Debola Adedapo <niikee80@hotmail.com>
2024-02-15 21:06:10 +0000

    typescript environment set up

---
+++ bowling-score.test.ts
@@ -0,0 +1,17 @@
+import { calculateBowlingScore } from './bowling-score'
+
+describe('calculateBowlingScore', () => {
+    // it('throws an error when the input/arg is not an integer', () => {
+    //     expect(() => calculateBowlingScore(12.3)).toThrow('Number is required')
+    // })
+    // it('throws an error when the input/arg is 0', () => {
+    //     expect(() => calculateBowlingScore(0)).toThrow(
+    //         'Number 0 cannot be passed',
+    //     )
+    // })
+    // it('returns the Roman Numeral Value of the inputed integer', () => {
+    //     expect(calculateBowlingScore(33)).toBe('XXXIII')
+    //     expect(calculateBowlingScore(2012)).toBe('MMXII')
+    //     expect(calculateBowlingScore(1996)).toBe('MCMXCVI')
+    // })
+})
---
+++ bowling-score.ts
@@ -0,0 +1,5 @@
+export function calculateBowlingScore(scoreArr: Array<number>): number {
+    let result = 0
+
+    return result
+}
